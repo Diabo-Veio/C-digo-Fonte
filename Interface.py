1dcc10cbcffb990326e0a62960e12475fdf3d2cf
Diabo Veio <guilhermefranceschi1@gmail.com>
2025-08-19 14:14:44 -0300

    first commit

---
+++ Interface.py
@@ -0,0 +1,118 @@
+import tkinter as tk
+from tkinter import PhotoImage
+from ORCA_INP import *
+from tkinter.ttk import *
+from tkinter import messagebox
+import sys
+import os
+
+## GAMBIARRA PARA PEGAR O CAMINHO ABSOLUTO DA IMAGEM E ADICIONAR O .EXE ##
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
+
+#-----------------------------------------------------------------------------------#
+
+## CRIA INSTANCIA DO TKINTER ##
+root = tk.Tk()
+root.title("Pontuador")
+
+## VARIAVEL PARA DEFINIR O METODO E NUMERO DE NUCLEOS##
+Metodo = tk.IntVar(value=0)
+Alterar_Nucleos = tk.BooleanVar(value=False)
+
+## VARIÁVEIS GLOBAIS ##
+Numero_Atm = ""
+arquivos = []
+
+#-----------------------------------------------------------------------------------#
+
+## ETIQUETA PARA IDENTIFICAR O CAMPO DE DIGITAÇÃO ##
+label1 = tk.Label(root,text="Número de Átomos").grid(column=0,row=0)
+
+## CAMPO PARA DIGITAR O NUMERO DE ATOMOS ##
+entrada1 = tk.Entry(root)
+entrada1.grid(column=0,row=1)
+
+## BOTÕES PARA ESCOLHA DO METODO ##
+tk.Radiobutton(root, text="Dimero Sem Iodo", variable= Metodo,value=1).grid(column=0,row=2)
+tk.Radiobutton(root, text="Cluster Sem Iodo", variable= Metodo,value=2).grid(column=0,row=3)
+tk.Radiobutton(root, text="Dimero com Iodo", variable= Metodo,value=3).grid(column=0,row=4)
+tk.Radiobutton(root, text="Cluster com Iodo", variable= Metodo,value=4).grid(column=0,row=5)
+
+## BOTÃO PARA ESCOLHER O ARQUIVO ##
+button1 = tk.Button(root,text="selecionar arquivos", command= lambda:Escolher_Arquivo()).grid(column=0,row=6)
+
+## BOTÃO PARA ALTERAR O NUMERO DE NÚCLEOS ##
+tk.Checkbutton(root,text="Alterar número de nucleos", variable = Alterar_Nucleos,command=lambda:Alt_Nucleos()).grid(column=0,row=7)
+entrada2 = tk.Entry(root)
+
+## BOTÃO GERAR OS OUTPUTS ##
+button2 = tk.Button(root,text="Gerar Arquivos", command= lambda:Gerar()).grid(column=0,row=9)
+
+#-----------------------------------------------------------------------------------#
+
+## MOSTRA/ESCONDE CAMPO PARA MUDANÇA DE NÚCLEOS
+def Alt_Nucleos():
+    if(Alterar_Nucleos.get()):
+        entrada2.grid(column=0,row=8)
+    else:
+        entrada2.grid_forget()
+
+## METODO PARA CHAMAR A EDIÇÃO DE STRINGS ##
+def Escolher_Arquivo():
+    global arquivos, Numero_Atm
+    ## LÊ O NUMERO DE ATOMOS
+    Numero_Atm = entrada1.get()
+    ## VERIFICA SE O NUMERO DE ATOMOS NÃO É NULO E CHAMA UM ERRO SE FOR ##
+    if Numero_Atm != "":
+        ## ABRE OS ARQUIVOS ##
+        arquivos = abrir()
+    else:
+       Nova_Aba()
+
+## ERRO DO FAUSTÃO ##
+class Nova_Aba():
+    def __init__(self):
+        self.abrir()
+    def abrir(self):
+        ## ABRE NOVA ABA ##
+        self.new_window = tk.Toplevel()
+        self.new_window.title("ERRRRROOOU")
+        self.new_window.geometry("253x253") 
+        ## ABRE A IMAGEM E MUDA O TAMANHO ##
+        self.image_orig = PhotoImage(file=resource_path("erro.png"))
+        self.image = self.image_orig.subsample(2, 2)
+        ## COLOCA A IMAGEM NA TELA ##
+        self.image_label = tk.Label(self.new_window, image=self.image)
+        self.image_label.grid(column=0,row=0)
+        self.image_label.image = self.image
+
+## METODO PARA CHAMAR A EDIÇÃO DE STRINGS ##
+def Gerar():
+    global Arquivos_gerados
+    ## DEFINE OS NUCLEOS COMO ZERO  SÓ PRA PASSAR COMO ARGUMENTO SE O ALTERAR_NUCLEOS FOR FALSO ##
+    Nucleos = 0
+    ## DEFINE O NUMERO DE NÚCLEOS SE ALTERAR_NUCLEOS FOR VERDADEIRO ##
+    if(Alterar_Nucleos):
+        Nucleos = entrada2.get()
+    ## CHAMA O METODO UM VEZ POR ARQUIVO ##
+    Arquivos_gerados = []
+    for arquivo in arquivos:
+        ## LÊ O CAMINHO PARA O ARQUIVOS ##
+        f = open(arquivo)
+        ## LÊ O CONTEUDO DO ARQUIVO EM QUESTÃO ##
+        Molecula = f.read()
+        ## CHAMA O METODO PARA EDIÇÃO DAS LINHAS ##
+        a = String_Manipulation(Molecula,arquivo,int(Numero_Atm),Metodo.get(),Alterar_Nucleos.get(),Nucleos)
+        if a == 1:
+            messagebox.showerror(title="Erro",message="Selecione o arquivo correto")
+    ## CRIA O EXECUTÁVEL PARA CHAMAR O ORCA ##
+    Executavel()
+
+def aviso():
+    messagebox.showerror(title="LEMBRE-SE",message="ESSE PROGRAMA DEVE SER USADO DIRETAMENTE EM ARQUIVOS GERADOS PELO AVOGRADO")
+## INICIA O LOOP DA INSTANCIA DO TKINTER ##
+aviso()
+root.mainloop()
---
+++ ORCA_INP.py
@@ -0,0 +1,184 @@
+from tkinter.filedialog import askopenfilenames
+from io import StringIO
+from string import ascii_uppercase as alc
+import os
+import errno
+
+## VARIAVEL GLOBAL PARA ARMAZENAR O NOME DOS ARQUIVOS ##
+Arquivos_gerados = []
+caminho = ""
+
+## ABRE O ARQUIVO ##
+def abrir():
+    filename = askopenfilenames()
+    return filename
+
+
+def String_Manipulation(Moleculas,nome,Numero_Atm,Metodo,Alterar_Nucleos,Nucleos):
+    global Molecula, Molecula_Alvo
+    ## REMOVE O METODO INICIAL E O SEPARA O * QUE MARCA O INICIO DOS ATOMOS PARA DICIONARMOS DEPOIS ##
+    if Moleculas[:73] == '''# avogadro generated ORCA input file 
+# Basic Mode
+# 
+! RHF SP def2-SVP 
+''':
+        inicio = Moleculas[73:84]
+        Mol_Limpas = Moleculas[84:]
+
+        ## DEFINE O METODO ##
+        
+        if Metodo == 1:
+            Metodo_escolhido = '''! SP wB97X-d3 RIJCOSX def2-tzvp def2/J def2-tzvp/C TightSCF
+%pal nprocs 22 end 
+%maxcore 3000
+%scf Guess Pmodel end'''
+        elif Metodo == 2:
+            Metodo_escolhido ='''! SP wB97X-d3 RIJCOSX def2-tzvp def2/J def2-tzvp/C TightSCF
+%pal nprocs 19 end 
+%maxcore 3000
+%scf Guess Pmodel end'''
+        elif Metodo == 3:
+            Metodo_escolhido ='''! SP wB97X-d3 RIJCOSX def2-tzvp def2/J def2-tzvp/C TightSCF
+%pal nprocs 22 end 
+%maxcore 3000
+%scf Guess Hueckel end'''
+        elif Metodo == 4:
+            Metodo_escolhido ='''! SP wB97X-d3 RIJCOSX def2-tzvp def2/J def2-tzvp/C TightSCF
+%pal nprocs 19 end 
+%maxcore 3000
+%scf Guess Hueckel end'''
+
+        ## DEFINE O NUMERO DE MOLECULAS A VERIFICAR ##
+        Numero_Mol = (Mol_Limpas.count("\n")-2)/Numero_Atm
+
+        ## CONTADORES ##
+        Atm = 0
+        i = 0
+        Molecula_Alvo = 0
+        Molecula = 1
+
+        ## CHAMA O METODO PARA EDITARMOS OS ARQUIVOS ##
+        for i in range(int(Numero_Mol)):
+            Molecula_Alvo += 1
+            sla = StringIO(Mol_Limpas)
+            Manipulation(Numero_Atm,Atm,sla,Metodo_escolhido,inicio,nome,alc[i],Alterar_Nucleos,Nucleos)
+            i+=1
+        ## CRIA UMA CÓPIA DO ARQUIVO ORIGINAL NA PASTA DE RESULTADOS ##
+        sla = StringIO(Mol_Limpas)
+        Copia(inicio,sla,Metodo_escolhido,Alterar_Nucleos,Nucleos,nome)
+        return 0
+    else:
+        return 1
+
+## METODO PARA EDIÇÃO DE STRINGS ##
+def Manipulation(Numero_Atm,Atm,sla,Metodo,inicio,nome1,letra,Alterar_Nucleos,Nucleos):
+    global Molecula, Arquivos_gerados,caminho
+    ## LISTAS PARA INSERIRMOS AS LINHAS ##
+    lista = []
+    lista2 = []
+    lista3 = []
+    Molecula = 1
+    ## DEFINE O NOME DO ARQUIVO ##
+    nome = nome1.removesuffix(".inp")
+    res = nome.split("/", -1)
+    nome_certo = res[-1] if len(res) > 1 else ""
+    ## DEFINE O CAMINHO PARA O ARQUIVO ##
+    caminho = ''
+    res.remove(nome_certo)
+    for i in res:
+        caminho += i + '/'
+    nome_completo = nome_certo + letra + ".inp"
+
+    ## LOOP PARA EDIÇÃO DAS LINHAS ##
+    while True:
+        ## LÊ AS LINHAS ##
+        linha = sla.readline()
+        
+        ## VERIFICA SE CHEGAMOS AO FINAL NO ARQUIVO PARA PARAR A ITERAÇÃO ##
+        if linha == "*\n":
+            break
+
+        ## IDENTIFICA QUE CHEGAMOS NA PRÓXIMA MOLECULA ##
+        if Atm == Numero_Atm :
+            Molecula += 1
+            Atm = 0
+
+        ## ADICIONA A LINHA NA LISTA SE CHEGAMOS NA MOLECULA ALVO ##
+        if Molecula == Molecula_Alvo:
+            while True:
+                ## PASSA PARA O PRÓXIMO ÁTOMO ##
+                Atm += 1
+                ## ADICIONA LINHA NA LISTA ##
+                lista.append(linha)
+                ## VERIFICA SE ACABOU A MOLECULA, PASSA PARA A PROXIMA MOLECULA E ENCERRA O LOOP ##
+                if Atm == Numero_Atm:
+                    Atm = 0
+                    Molecula += 1
+                    break
+                ## LÊ A PROXIMA LINHA ##
+                linha = sla.readline()
+                
+        ## INICIA A EDIÇÃO SE NÃO ESTAMOS NA MOLECULA ALVO ##
+        elif Molecula < Molecula_Alvo or Molecula > Molecula_Alvo:
+            ## EDITA A LINHA DA MOLECULA##
+            linha_rep = linha.replace(linha[3:5],linha[3]+":")
+            ## ADICIONA A LINHA DA MOLECULA NA LISTA CORRETA ##
+            if  Molecula < Molecula_Alvo:
+                lista2.append(linha_rep)
+            elif  Molecula > Molecula_Alvo:
+                lista3.append(linha_rep)
+            ## PASSA PARA O PRÓXIMO ÁTOMO ##
+            Atm += 1
+    
+    ##VERIFICA SE JA EXISTE UMA PASTA DE RESULTADOS ##
+    if not os.path.exists(os.path.dirname(caminho + "Resultados/")):
+        try:
+            ## CRIA A PASTA DE RESULTADOS SE NÃO EXISTIR ##
+            os.mkdir(os.path.dirname(caminho + "Resultados/"))
+        except OSError as exc:
+            ## CASO DE ALGUM ERRO ##
+            if exc.errno != errno.EEXIST:
+                raise
+    
+    ##EDITA O METODO
+    if(Alterar_Nucleos):
+        Novo_Metodo = Metodo.replace(Metodo[72:74], Nucleos)
+
+    ## ESCREVE O ARQUIVO ##
+    with open(caminho + "Resultados/" + nome_completo,"w") as outfile:
+        Arquivos_gerados.append(nome_completo)
+        if (Alterar_Nucleos):
+            outfile.write(Novo_Metodo)
+        else:
+            outfile.write(Metodo)
+        outfile.write(inicio)
+        outfile.writelines([str(i) for i in lista2])
+        outfile.writelines([str(i) for i in lista])
+        outfile.writelines([str(i) for i in lista3])
+        outfile.writelines("*\n")
+
+## COPIA O ARQUIVO QUE ESTÁ SENDO EDITADO PARA A PASTA DE RESULTADOS ##
+def Copia(inicio,Moleculas,Metodo,Alterar_Nucleos,Nucleos,nome):
+    global Arquivos_gerados
+    ## DEFINE O NOME DO ARQUIVO ##
+    res = nome.split("/", -1)
+    nome_certo = res[-1] if len(res) > 1 else ""
+    ## VERIFICA SE HOUVE MUDANÇA NA QUANTIDADE DE NÚCLEOS ##
+    if(Alterar_Nucleos):
+        Novo_Metodo = Metodo.replace(Metodo[72:74], Nucleos)
+    ## ESCREVE O ARQUIVO DE SAIDA ##
+    with open(caminho + "Resultados/" + nome_certo,"w") as outfile:
+        Arquivos_gerados.append(nome_certo)
+        if (Alterar_Nucleos):
+            outfile.write(Novo_Metodo)
+        else:
+            outfile.write(Metodo)
+        outfile.write(inicio)
+        outfile.writelines(Moleculas)
+
+## GERA O EXECUTAVEL PARA O ORCA ##
+def Executavel():
+    with open(caminho + "Resultados/" + "Energias" + ".ps1","w") as outfile:
+        for i in Arquivos_gerados:
+            out = i.removesuffix(".inp")
+            outfile.writelines("C:\orca503\orca.exe " + i + ">" + out + ".out \n")
